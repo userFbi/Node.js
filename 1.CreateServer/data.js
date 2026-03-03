8c4ea21ba9fe8e7faa60f6c7e2e5918f36b41da4
userFbi <pawartushar2024.katargam@gmail.com>
2026-03-03 09:50:45 +0530

    learned file structure

--- 1.CreateServer/data.js
+++ 1.CreateServer/data.js
@@ -1,11 +1,16 @@
+const { error } = require("console");
 const http = require("http");
 const app = http.createServer((req, res) => {
   if (req.url == "/") {
     res.write("This is Home");
+    res.statusCode = 200;
   } else if (req.url == "/about") {
     res.write("This is About");
   } else if (req.url == "/contact") {
     res.write("This is Contact");
+  } else {
+    res.statusCode = 404;
+    res.write("Page Not Found 404");
   }
   res.end();
 });
--- revision/data.js
+++
@@ -1,6 +0,0 @@
-const http = require("http");
-const app = http.createServer((req, res) => {
-  res.write("This is Home")
-  res.end();
-});
-app.listen(3000);
